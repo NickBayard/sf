826b517b52d4dbb0aa49d3f5a9eafccbc5293caf
Nick Bayard <nbayard@gmail.com>
2017-07-10 21:00:55 -0600

    [master] Monitor now checks for heartbeat in 1 second intervals during its idle time

--- storage_monitor.py
+++ storage_monitor.py
@@ -5,6 +5,7 @@ import os
 import time
 import subprocess
 import re
+import math
 from datetime import datetime
 
 from storage_object import StorageObject
@@ -57,15 +58,31 @@ class StorageMonitor(StorageObject):
                                         type='MONITOR',
                                         payload=process))
 
-            # Check for the heartbeat or kill message before sleeping
-            if not self.check_heartbeat():
-                break
-
             # Subtract the elapsed time from the poll period for more accurate
             # monitor polling intervals
             sleep_time = self.poll_period - (time.time() - monitor_start)
+
             if sleep_time > 0:
-                time.sleep(sleep_time)
+                # Check for the heartbeat or kill message before sleeping
+                if not self.check_heartbeat():
+                    break
+
+                # Sleep check the heartbeat in 1 second intervals
+                # First sleep the remaining fraction
+                time.sleep(sleep_time - math.floor(sleep_time))
+
+                # We need this so that we can break out of the for/range loop
+                # and then break out of the while loop again
+                alive = True
+
+                for _ in range(int(math.floor(sleep_time))):
+                    if not self.check_heartbeat():
+                        alive = False
+                        break
+                    time.sleep(1)
+
+                if not alive:
+                    break
 
         # Report that monitor has stopped running
         self.heartbeat.send(Message(name='Monitor',
