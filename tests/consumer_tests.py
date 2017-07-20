36b6f50c00ffbb752064b3c8153c6583287efbcf
Nick Bayard <nbayard@gmail.com>
2017-07-19 21:34:34 -0600

    [master] Added file for StorageConsumer unittests

---
+++ tests/consumer_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from client import StorageConsumer
+
+
+class TestConsumer(unittest.TestCase):
+    def test_chunk_write_timing(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
