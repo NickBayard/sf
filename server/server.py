404d9ddc5313f5683b820dcebb364ee8c1715278
Nick Bayard <nbayard@gmail.com>
2017-07-25 22:34:15 -0600

    [master] Started rewriting the report.

--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 """Contains the definitions for the Server class"""
 
+import pdb
 import os.path
 import threading
 import multiprocessing
+
 from Queue import Queue, Empty
 from datetime import datetime
+from collections import namedtuple
 
 from shared import configure_logging, init_dir_path
 from SocketServer import TCPServer
@@ -26,8 +29,8 @@ class ClientData(object):
             message_queue: A "managed" queue for the handling_process
                 to forward Messages back to the queue_thread.
 
-            messages: A list of messages that have been processed by
-                queue_thread.
+            messages: A dict of messages that have been processed by
+                queue_thread. The dict is keyed by message type.
 
             kill: A "managed" Event that lets the server stop processes
                 and threads from running.
@@ -68,7 +71,7 @@ class ClientData(object):
         self.queue_thread.start()
 
         # Messages processed by queue_thread will be stored here.
-        self.messages = []
+        self.messages = {}
 
         # Track the status of each client connection
         self.done = False
@@ -142,6 +145,9 @@ class Server(MultiprocessMixin, TCPServer):
         clients: A dict with client address tuples as keys and ClientData
             as values.
     """
+
+    # This namedtuple is used when processing the messages in the report
+    ID = namedtuple('ID', 'name id')
 
     allow_reuse_address = True
 
@@ -232,7 +238,7 @@ class Server(MultiprocessMixin, TCPServer):
                                                               client_address,
                                                               dispatch_string))
 
-        client.messages.append(message)
+        client.messages.setdefault(message.type, []).append(message)
 
     def _handle_aggregate_response(self, message, client):
         """Generate a response string for Messages that have payloads with
@@ -339,6 +345,116 @@ class Server(MultiprocessMixin, TCPServer):
 
         return True
 
+    def _report_runtime(self, file, start, stop):
+
+        class StartStop(object):
+            def __init__(self, start=None, stop=None):
+                self.start = start
+                self.stop = stop
+
+        file.write('  Runtime:\n')
+        # Verify that we received a START and STOP message
+        if start is not None and stop is not None:
+            # We should get a start and stop message from each client process
+            # Build a dictionary containing start and stop messages from
+            # each child process
+            messages = {}
+            for s in start:
+                process = self.ID(name=s.name, id=s.id)
+
+                if process in messages:
+                    file.write('    ERROR: Multiple START messages detected ')
+                    file.write('for {}_{}\n'.format(process.name, process.id))
+                else:
+                    messages[process] = StartStop(start=s)
+
+            # There should only be 1 stop message
+            if len(stop) > 1:
+                file.write('    ERROR: Multiple STOP messages detected ')
+            else:
+                stop_received = stop[0].payload[0]
+                stop_missing = stop[0].payload[1]
+
+                # Add START messages
+                for s in stop_received:
+                    process = self.ID(name=s.name, id=s.id)
+                    if process in messages:
+                        if messages[process].stop is None:
+                            messages[process].stop = s
+                        else:
+                            file.write('    ERROR: Multiple STOP messages detected ')
+                            file.write('for {}_{}\n'.format(process.name, process.id))
+                    else:
+                        file.write('   ERROR: STOP message present without START ')
+                        file.write('for {}_{}\n'.format(process.name, process.id))
+                        messages[process].stop = StartStop(stop=s)
+
+                # Report on missing stop messages
+                for s in stop_missing:
+                    process = self.ID(name=s[0], id=s[1])
+                    if process in messages:
+                        file.write('    ERROR: START message present without STOP ')
+                        file.write('for {}_{}\n'.format(process.name, process.id))
+                    else:
+                        file.write('    ERROR: START and STOP messages missing ')
+                        file.write('for {}_{}\n'.format(process.name, process.id))
+
+            # Iterate through the messages and print the start/stop/runtimes
+            for process, message in messages:
+                if message.stop is None:
+                    # If a START is present and a STOP is not, then skip it, we've
+                    # already written an error.
+                    continue
+
+                file.write('    {}_{}:\n'.format(process.name, process.id))
+
+                if message.start is not None:
+                    file.write('      Start: {}\n'.format(message.start.date_time))
+
+                if message.stop is not None:
+                    file.write('      Stop: {}\n'.format(message.stop.date_time))
+
+                if message.start is not None and message.stop is not None:
+                    runtime = message.stop.date_time - message.start.date_time
+                    file.write('      Runime: {}\n\n'.format(runtime))
+
+        elif start is None:
+            # Didn't get a start message
+            file.write('    ERROR: START messages not received.\n')
+        elif stop is None:
+            # Didn't get a stop message
+            file.write('    ERROR: STOP messages not received.\n')
+
+    def _report_heartbeat(self, file, messages):
+        # HEARTBEAT messages are aggregated
+        # Let's regroup them by child process
+        heartbeat_messages = {}
+
+        for message in messages:
+            for child_process in message.payload[0]:
+                process = ID(name=child_process.name, id=child_process.id)
+                heartbeat_messages.setdefault(process, []).append(child_process.date_time)
+
+            # Append None for missing heartbeat
+            for name, id in message.payload[1]:
+                process = ID(name=name, id=id)
+                heartbeat_messages.setdefault(process, []).append('ERROR: Missing heartbeat')
+
+        file.write('\n')
+        file.write('  Heartbeat:\n')
+        # Now iterate through heartbeat_messages which have heartbeat responses and
+        # missed responses grouped by child process
+        for process, heartbeats in heartbeats.iteritems():
+            file.write('    {}_{}:\n'.format(process.name, process.id))
+            for heartbeat in heartbeats:
+                file.write('      {}\n'.format(heartbeat))
+
+    def _report_rollover(self, file, messages):
+        pass
+
+    def _report_monitor(self, file, messages):
+        pass
+
     def _generate_report(self):
         """Generate a report on all previously connected clients."""
         filename = 'Server_Report_{}.log'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))
@@ -346,9 +462,20 @@ class Server(MultiprocessMixin, TCPServer):
 
         with open(filepath, 'w') as file:
             for address, client in self.clients.iteritems():
-                file.write('Client @ {}:{}\n'.format(address[0], address[1]))
+                file.write('Client @ {}:{}\n\n'.format(address[0], address[1]))
 
-                for message in client.messages:
-                    file.write(repr(message) + '\n')
+                pdb.set_trace()
+                self._report_runtime(file=file,
+                                     start=client.messages.get('START'),
+                                     stop=client.messages.get('STOP'))
+
+                self._report_heartbeat(file=file,
+                                       messages=client.messages.get('HEARTBEAT'))
+
+                self._report_rollover(file=file,
+                                      messages=client.messages.get('ROLLOVER'))
+
+                self._report_monitor(file=file,
+                                     messages=client.messages.get('MONITOR'))
 
                 file.write('\n')
